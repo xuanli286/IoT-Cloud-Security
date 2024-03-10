60f36ec91a5b11966fd0fe729a84a176f18bb6f4
Tan Yi Peng <tanyipeng834@gmail.com>
2024-03-10 16:12:09 +0800

    Changed the code

--- receiver.py
+++ receiver.py
@@ -8,12 +8,13 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("raspi/data")  # Subscribe to the desired topic upon connection
 
 def on_message(client, userdata, msg):
-    print("Received message: " + msg.payload.decode())
-    write_to_file(msg.payload.decode())  # Call function to write message to a text file
+    print("Message Received")
+    write_to_file(msg.payload)  # Call function to write message to a text file
 
 def write_to_file(message):
-    with open("received_data.txt", "a") as file:
-        file.write(message + "\n")
+    with open("received_data.cpabe","wb") as file:
+        file.write(message)
+
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
 client.on_connect = on_connect
@@ -23,3 +24,5 @@ client.tls_insecure_set(True)
 client.connect("a1u7o7eqy96ipy-ats.iot.us-east-1.amazonaws.com", 8883, 60)
 
 client.loop_forever()
+
+
